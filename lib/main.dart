1f46a62cc5a12c433769c0da375d106543b58fe6
sakshi3326 <saqkshiiitian123@gmail.com>
2022-09-05 00:49:25 +0530

    adding icon in app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app2/second.dart';
 
-void main() => runApp( first()); 
+void main() => runApp(first());
 
-class first extends StatelessWidget{
+class first extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Flutter App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Title'),
-         ),
-         body: Second()
-      )
-    );
-      
+        debugShowCheckedModeBanner: false,
+        title: 'My Flutter App',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Title'),
+              actions: <Widget>[Icon(Icons.access_alarm_rounded)],
+            ),
+            body: Second()));
   }
-
 }
